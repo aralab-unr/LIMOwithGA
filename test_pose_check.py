c324bd124d45eca9957aaa2525f07901c7c9e03d
Adarsh Sehgal <adarsh23.sehgal@gmail.com>
2019-02-12 10:01:08 -0800

    test pose check file changes

--- test_pose_check.py
+++ test_pose_check.py
@@ -1,8 +1,8 @@
 import os
 
 num_lines = sum(1 for line in open('/tmp/poses_dump.txt'))
-if num_lines > 1101:
-    print("modifying poses dump to 1101 poses")
+if num_lines > 1098:
+    print("modifying poses dump to 1098 poses")
     file = open('/tmp/poses_dump.txt', "r+", encoding = "utf-8")
 
     #Move the pointer (similar to a cursor in a text editor) to the end of the file. 
