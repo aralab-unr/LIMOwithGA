7e60dc5220cedb32ae55c8da234d8cee956db2ca
Adarsh Sehgal <adarsh23.sehgal@gmail.com>
2019-02-11 19:37:19 -0800

    new script to test GA found parameters on various sequences for required number of runs

---
+++ test_sequence_script.py
@@ -0,0 +1,22 @@
+import os
+
+sequence_number = 01
+num_runs = 10
+if os.path.isfile('/tmp/testfitnesses_dump_' + sequence_number + '.txt'):
+    os.system("rm -f /tmp/testfitnesses_dump_" + sequence_number + ".txt")
+
+for x in range(1, num_runs):
+    query = "./test_script.sh"
+    os.system(query)
+
+    # read fitness value as root mean square value (rmse) from text file
+    file = open('/tmp/rmse_output.txt', 'r')
+    rmse = float(file.read())
+
+    print('Saving fitnesses for each evaluation')
+    with open('/tmp/testfitnesses_dump_' + sequence_number + '.txt', 'a') as output:
+        output.write("" + str(timesEvaluated) + " " + str(rmse) + "\n") 
+
+    os.system("rm -f /tmp/rmse_output.txt")
+
+
